f1106d12276f168dda255d4a5584ab7000fdd503
Revdovo <63352181+Revdovo@users.noreply.github.com>
2025-03-29 20:19:32 +0700

    Delete client directory

--- client/components/footer.php
+++
@@ -1,7 +0,0 @@
-<!-- components/footer.php -->
-<footer class="bg-dark text-white text-center py-3">
-    © 2025 Website tính toán thông số hộp giảm tốc | Designed by Nhóm 1
-</footer>
-
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
