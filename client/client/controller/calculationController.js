747c41fc3e0a19b57faa3c82635a07a78c7152e2
Revdovo <63352181+Revdovo@users.noreply.github.com>
2025-04-27 15:52:12 +0700

    Delete client/client directory

--- client/client/controller/calculationController.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    let currentStep = localStorage.getItem("lastStep") ? parseInt(localStorage.getItem("lastStep")) : 1;
-    let calculationResults = [];
-
-    
-});
